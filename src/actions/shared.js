17ce691bfae303e0c99583a710542b20aeee4d81
akashsingh96 <akash.96317@gmail.com>
2019-09-20 16:12:26 +0530

    building leaderboard

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,18 @@ import { getInitialData } from "../utils/api";
 import receiveUsers from "./users";
 import receivePolls from "./polls";
 import setAuthedUser from "./authed";
+import { showLoading, hideLoading } from "react-redux-loading";
 
 const authedId = "tylermcginnis";
 
 export function handleInitialData() {
   return dispatch => {
+    dispatch(showLoading());
     getInitialData().then(({ users, polls }) => {
       dispatch(receiveUsers(users));
       dispatch(receivePolls(polls));
       dispatch(setAuthedUser(authedId));
+      dispatch(hideLoading());
     });
   };
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import LoadingBar from "react-redux-loading";
 
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
+import Leaderboard from "./Leaderboard";
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return this.props.loading ? null : (
+    return (
       <div>
-        <Dashboard />
+        <LoadingBar />
+        {this.props.loading === true ? null : <Leaderboard />}
       </div>
     );
   }
