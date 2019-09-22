145ce5365d7a044fcaacc0024801cf34d787b150
akashsingh96 <akash.96317@gmail.com>
2019-09-23 04:01:57 +0530

    Poll component UI

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,9 +1,53 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { getPercentage } from "../utils/helpers";
 
 class Poll extends Component {
+  handleAnswer = answer => {
+    const { poll, authedUser } = this.props;
+    this.answered = true;
+    console.log(answer);
+  };
   render() {
-    return <div className="poll-container">{JSON.stringify(this.props)}</div>;
+    if (!this.props.poll) return <p>This poll does not exit</p>;
+    const { poll, vote, authorAvatar } = this.props;
+    const total = ["aVotes", "bVotes", "cVotes", "dVotes"].reduce(
+      (total, key) => (total = total + poll[key].length),
+      0
+    );
+    // debugger;
+    return (
+      <div className="poll-container">
+        <h1 className="question">{poll.question}</h1>
+        <div className="poll-author">
+          By <img src={authorAvatar} alt="Author's avatar" />
+        </div>
+        <ul>
+          {["aText", "bText", "cText", "dText"].map(key => {
+            const count = poll[key[0] + "Votes"].length;
+            return (
+              <li
+                onClick={() => {
+                  if (!vote && !this.answered) return this.handleAnswer(key[0]);
+                }}
+                className={`option ${vote === key[0] ? "chosen" : ""}`}
+              >
+                {vote === null ? (
+                  poll[key]
+                ) : (
+                  <div className="result">
+                    <span>{poll[key]}</span>
+                    <span>
+                      {getPercentage(count, total)}%({count})
+                    </span>
+                  </div>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 
